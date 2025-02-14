cfb1e20f77b2d17e33d6a186b5788dee3054db2e
sleepbetterunaware <devmehta211@gmail.com>
2025-02-14 21:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ $(document).ready(function() {
     var btn_reset = $("#reset");
     var btn_yes = $("#yes");
     var btn_no = $("#no");
+    var audio1 = document.getElementById("audio1");
 
+    // Play audio on user interaction
     envelope.click(function() {
         open();
+        audio1.play(); // Play audio when envelope is clicked
     });
     btn_open.click(function() {
         open();
+        audio1.play(); // Play audio when open button is clicked
     });
     btn_reset.click(function() {
         close();
