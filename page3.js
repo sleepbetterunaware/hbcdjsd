b1a545b99e5157d4c247279b4afcc90f88dfc567
sleepbetterunaware <devmehta211@gmail.com>
2025-02-14 19:41:17 +0530

    Create page3.js

---
+++ page3.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var container = $("#images-container");
+    var btn_yes = $("#yes");
+
+    pdfjsLib.getDocument('pdf2').promise.then(function(pdf) {
+        for (var i = 1; i <= pdf.numPages; i++) {
+            pdf.getPage(i).then(function(page) {
+                var scale = 1.5;
+                var viewport = page.getViewport({ scale: scale });
+                var canvas = document.createElement('canvas');
+                var context = canvas.getContext('2d');
+
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+
+                page.render(renderContext).promise.then(function() {
+                    var imgData = canvas.toDataURL('image/png');
+                    var img = $('<img>').attr('src', imgData).css({
+                        position: 'absolute',
+                        top: Math.random() * 100 + '%',
+                        left: Math.random() * 100 + '%',
+                        width: '100px'
+                    }).appendTo(container);
+                });
+            });
+        }
+    });
+
+    btn_yes.click(function() {
+        window.location.href = 'page2.html';
+    });
+});
